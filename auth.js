e758f957d40f99e5befac562c69d60544e405a68
Her-divanni <jannsenherdivanni1@gmail.com>
2026-03-09 23:34:06 +0300

    auth.js

--- auth.js
+++ auth.js
@@ -2,6 +2,16 @@ import { auth, db } from "./firebase.js";
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCRMcs2PvqRbxIVsDss5kQRJe8mD840cBM",
+  authDomain: "chat-f35b2.firebaseapp.com",
+  projectId: "chat-f35b2",
+  storageBucket: "chat-f35b2.firebasestorage.app",
+  messagingSenderId: "1006603779080",
+  appId: "1:1006603779080:web:3d6c4d83af8365e23665bd",
+  measurementId: "G-SVL5NXG3C9"
+}
+
 const app = initializeApp(firebaseConfig);
 
 import {
@@ -43,3 +53,4 @@ window.login = async function(){
     window.location="chat.html";
 
 }
+
